591c44417f36e140e25b9d5742d45b161e2eb62f
FGohlke <fredericgohlke@web.de>
2024-08-26 22:42:03 +0200

    Ship is moving and Asteroids are moving and Collision can be detected

--- main.py
+++ main.py
@@ -3,20 +3,44 @@
 # throughout this file
 import pygame
 from constants import *
+from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 def main():
     pygame.get_init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    keepGameRunning=True
-    while keepGameRunning:
-       for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            return
-       screen.fill("black")
-       pygame.display.flip()
-    print("Starting asteroids!")
-    print(f"Screen width: { SCREEN_WIDTH}")
-    print(f"Screen height: { SCREEN_HEIGHT}")
+    keepGameRunning=True    
+    delta_time = pygame.time.Clock()
+    player1 = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)       
+    dt = 0
+    updatable = pygame.sprite.Group()
+    drawable  = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable)
+    Player.containers = (updatable, drawable)
+    asteroidfield1 = AsteroidField()
+    updatable.add(player1)
+    updatable.add(asteroidfield1)
+    drawable.add(player1)
+    
+    
+    while keepGameRunning:              
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return 
+        screen.fill("black")
+        for item in drawable:
+            item.draw(screen)        
+        for item in updatable:
+            item.update(dt)
+        for item in asteroids:            
+            if item.check_collision(player1):
+                print("Game  over!")
+                raise SystemExit              
+        pygame.display.flip() 
+        dt = delta_time.tick(60) / 1000              
 
 if __name__ == "__main__":
     main()
